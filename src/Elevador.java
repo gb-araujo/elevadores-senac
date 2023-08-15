e04b159d5c25d47fa8c630b2abbc7c7d5e32edf0
Gabriel Araujo <86437334+gb-araujo@users.noreply.github.com>
2023-08-15 16:57:10 -0300

    Update Elevador.java

--- src/Elevador.java
+++ src/Elevador.java
@@ -7,37 +7,39 @@ public class Elevador {
     int quantidadePessoas;
     int andaresAtendidos;
 
-    public Elevador(String local, int andarAtual, int totalAndares, int capacidade, int quantidadeMaxPessoas, int quantidadePessoas) {
+    public Elevador(String local, int andarAtual, int totalAndares, double capacidade, int quantidadeMaxPessoas, int quantidadePessoas) {
         this.local = local;
         this.andarAtual = andarAtual;
         this.totalAndares = totalAndares;
         this.capacidade = capacidade;
         this.quantidadeMaxPessoas = quantidadeMaxPessoas;
         this.quantidadePessoas = quantidadePessoas;
-
     }
 
     public void subir(int incremento){
-        if (totalAndares > 5) {
-            System.out.println("O ultimo andar é 5");
-        } else {
-            this.andarAtual += incremento;
+        int novoAndar = andarAtual + incremento;
+        if (novoAndar <= totalAndares && novoAndar <= 5) {
+            this.andarAtual = novoAndar;
             System.out.println("Subindo " + incremento + " Andares");
+        } else {
+            System.out.println("Não é possível subir até esse andar.");
         }
     }
 
     public void descer(int decremento){
-        if (totalAndares < -1) {
-            System.out.println("Só consegue descer até o primeiro subsolo.");
-        } else {
-            this.andarAtual -= decremento;
+        int novoAndar = andarAtual - decremento;
+        if (novoAndar >= 0 && novoAndar >= -1) {
+            this.andarAtual = novoAndar;
             System.out.println("Descendo " + decremento + " Andares");
+        } else {
+            System.out.println("Não é possível descer até esse andar.");
         }
     }
 
     public void addPessoa(int incremento){
-        if (quantidadePessoas < quantidadeMaxPessoas) {
-            quantidadePessoas += incremento;
+        int novaQuantidadePessoas = quantidadePessoas + incremento;
+        if (novaQuantidadePessoas <= quantidadeMaxPessoas && novaQuantidadePessoas <= capacidade) {
+            quantidadePessoas = novaQuantidadePessoas;
             System.out.println("Pessoa adicionada");
         } else {
             System.out.println("Capacidade máxima atingida");
@@ -45,13 +47,15 @@ public class Elevador {
     }
 
     public void removePessoa(int decremento){
-        if (this.quantidadePessoas > 0) {
-            quantidadePessoas -= decremento;
+        int novaQuantidadePessoas = quantidadePessoas - decremento;
+        if (novaQuantidadePessoas >= 0) {
+            quantidadePessoas = novaQuantidadePessoas;
             System.out.println("Pessoa removida");
         } else {
             System.out.println("O elevador está vazio.");
         }
     }
+
     public void imprimirDados() {
         System.out.println("Elevador: " + this.local);
         System.out.println("Andar atual: " + this.andarAtual);
@@ -62,4 +66,16 @@ public class Elevador {
         System.out.println("-------------------------------------------------------");
     }
 
+    public static void main(String[] args) {
+        // Exemplo de uso do elevador
+        Elevador elevador = new Elevador("Edifício A", 0, 10, 1000, 10, 0);
+
+        elevador.addPessoa(5);
+        elevador.subir(3);
+        elevador.imprimirDados();
+
+        elevador.removePessoa(3);
+        elevador.descer(2);
+        elevador.imprimirDados();
+    }
 }
